30c3c2a4e65b5e1ae3ce902614028b6df167879c
Marius <marius.vladoiu@gmail.com>
2020-07-13 19:48:40 +0300

    Package war instead of jar, Part2: https://www.baeldung.com/spring-boot-war-tomcat-deploy

--- src/main/java/com/wipro/training/TrainingApplication.java
+++ src/main/java/com/wipro/training/TrainingApplication.java
@@ -2,9 +2,10 @@ package com.wipro.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrainingApplication {
+public class TrainingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
